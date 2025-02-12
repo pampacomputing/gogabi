92112e41a966f9042bb5c896bf39fa527d7f2223
Francesco Galvão <francescogalvao@gmail.com>
2025-02-12 19:00:11 -0300

    Configure Next.js for GitHub Pages deployment; add build and setup workflows

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,11 @@
 import type { NextConfig } from "next";
-const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true, // Disable default image optimization
   },
-  assetPrefix: isProd ? '/gogabi/' : '',
-  basePath: isProd ? '/gogabi' : '',
+  assetPrefix:'/gogabi/',
+  basePath: '/gogabi',
   output: "export",
 
 };
