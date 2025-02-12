3607cf96eae143479176e5f8f46c41ff1d8d8a1e
Francesco Galvão <francescogalvao@gmail.com>
2025-02-12 18:46:03 -0300

    Configure Next.js for production deployment with asset prefix and base path; add homepage and deployment scripts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
-
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/gogabi/' : '',
+  basePath: isProd ? '/gogabi' : '',
   output: "export",
+
 };
 
 export default nextConfig;
